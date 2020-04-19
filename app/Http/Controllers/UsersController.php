a5093cd3b40381a6b436bef7edf0c248482642da
yuma628 <yuma28061998@gmail.com>
2020-04-19 10:40:07 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,9 +20,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
         
-        return view('users.show', [
+        $date = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+        
+        $date += $this->counts($user);
+        
+        return view('users.show', $date);
     }
 }
